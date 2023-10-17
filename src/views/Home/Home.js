d2e2d484659704d6d4b1174dffa409327ad1aacb
ganeshgore1811 <ganeshgore0909@gmail.com>
2023-10-17 21:41:05 +0530

    changes

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,49 +1,42 @@
-import axios from 'axios';
-import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import React, { useEffect,useState} from "react";
 import './Home.css';
 export default Home
 
-
 function Home(){
-    const [city, setCity] = useState('pune');
-    const [temperature, setTemperature] = useState(0)
-    const [message, setMessage] = useState('')
+    const[city, setCity] = useState('pune');
+    const[temperature, setTemperature] = useState(0)
+    const[message, setMessage] = useState(0)
 
     async function loadWeatherInfo(){
-    try{
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=61acad472d8a369cd3423bcd679e0498`)
+        try{
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=61acad472d8a369cd3423bcd679e0498`)
 
-        setTemperature((response.data.main.temp -273).toFixed(2))
-        setMessage('✅ Data Fetched Successfully...')
+            setTemperature(( response.data.main.temp-273).toFixed(2))
+            setMessage('✅Data Fetched Successfully...')
+        }
+        catch(err){
+            setTemperature(0)
+            setMessage('City Not Found')
+        }
     }
-    catch(err){
-        setTemperature(0) 
-        setMessage('City Not Found')
-    }
-    }   
-
     useEffect(()=>{
         loadWeatherInfo()
     },[city])
-
-    return (
+    return(
         <div>
-
-            <h1 className='app-title'> Weather For {city}</h1>
-
-           <input
-            className='search-bar'
-            type='text'
-            placeholder='Enter City...'
+            <h1 className="app-title"> Weather For { city}</h1>
+            <input className="search-bar"
+            type="text"
+            placeholder="Enter City..."
             value={city}
             onChange={(e)=>{
-            setCity(e.target.value)
+                setCity(e.target.value)
             }}
             />
-            <p className='message-text'>{message}</p>
-         
-           <h1 className='temprature-text'>Temperature: {temperature} °C</h1>
-               
-         </div>
+            <p className="message-text"> {message}</p>
+
+            <h1 className="temprature-text"> temperature:{temperature} ℃ </h1>
+        </div>
     )
 }
